7f292a5d8113cf89d34c737c099a993cd97c1c8e
Mikko Forsström <mikko.forsstrom@veikkaus.fi>
2024-01-10 12:41:29 +0200

    Puupero

---
+++ puupero.mjs
@@ -0,0 +1,55 @@
+import cx from "clsx";
+
+const LOOP_DE_LOOPS = 10;
+const ITERATIONS = process.env.ITERATIONS || 100000;
+
+const classes = ["pieru", "keisari"];
+
+const navigationOnTop = true;
+const showHeaderTitle = false;
+
+cx(
+  "carousel clearfix",
+  classes,
+  navigationOnTop && "carousel--navi-on-top",
+  !showHeaderTitle && "without-header"
+);
+
+// object
+
+performance.mark("object-start");
+for (let x = 1; x <= ITERATIONS; x = x + 1) {
+  cx("carousel clearfix", classes, {
+    "carousel--navi-on-top": navigationOnTop,
+    "without-header": !showHeaderTitle,
+  });
+}
+performance.mark("object-end");
+
+// no-object
+
+performance.mark("noobject-start");
+for (let x = 1; x <= ITERATIONS; x = x + 1) {
+  cx(
+    "carousel clearfix",
+    classes,
+    navigationOnTop && "carousel--navi-on-top",
+    !showHeaderTitle && "without-header"
+  );
+}
+performance.mark("noobject-end");
+
+const objectMeasure = performance.measure(
+  "object",
+  "object-start",
+  "object-end"
+);
+
+const noobjectMeasure = performance.measure(
+  "noobject",
+  "noobject-start",
+  "noobject-end"
+);
+
+console.log(objectMeasure);
+console.log(noobjectMeasure);
